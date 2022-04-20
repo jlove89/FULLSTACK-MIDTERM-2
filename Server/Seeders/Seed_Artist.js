877573612bbfbde9bc5102dcf2ede3597f33342c
ZacharyDick828 <91510605+ZacharyDick828@users.noreply.github.com>
2022-04-20 16:38:13 -0700

    connected to database

---
+++ Server/Seeders/Seed_Artist.js
@@ -0,0 +1,22 @@
+const db = require('./models')
+
+db.Artist.create([{
+    name: 'Kyle Partin',
+    Moniker: 'Cut Rugs',
+    art_type: 'Electronic',
+    genre: 'Exp. Bass',
+    phone: '888-888-8888',
+    email: 'boy@gmail.com',
+    agency: 'The Undergrowth',
+    links: "pewpew"
+}])
+.then(() => {
+    console.log('Success!')
+    process.exit()
+})
+.catch(err => {
+    console.log('Failure!', err)
+    process.exit()
+})
+
+
