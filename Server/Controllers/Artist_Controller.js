05013f13c7219d90e96bcb2e6e34937f4743b8ea
Jeremy Love <pristinesolutionsnc@gmail.com>
2022-04-20 21:19:50 -0400

    refactor

--- Server/Controllers/Artist_Controller.js
+++ Server/Controllers/Artist_Controller.js
@@ -1,29 +1,21 @@
-const findAllArtists = require('express').Router
+const router = require('express').Router()
 //const findArtistByArt_Type = require('express').Router
 //const findArtistByName = require('express').Router
-const db = require('../Models')
+const Artist = require('../Models/ArtistModel')
 
 // ROUTE 1
-findAllArtists.get('/', (req, res) => {
-    db.findAllArtists.find()
-    .then((findAllArtists) => {
-        res.render('Models/ArtistModel', { findAllArtists })
-    })
-    //.catch(err => {
-       // console.log('err', err)
-        //res.render('error404')
-   // })
+router.get('/', (req, res) => {
+
 })
 
 // ROUTE 2
-findAllArtists.post('/', (req,res) => {
-    db.findAllArtists.create(req.body)
-    //.then(() => {
-        //res.redirect('/Models')
-    //})
+router.post('/', (req,res) => {
+
 })
 
 // ROUTE 3
-findAllArtists.put('/', (req, res) => {
-    db.findAllArtists.findBye
+router.put('/', (req, res) => {
+
 })
+
+module.exports = router
