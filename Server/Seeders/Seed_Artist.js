a14681a4e1b181c664bc5c118713a7a22d97f4e9
Jeremy Love <pristinesolutionsnc@gmail.com>
2022-04-25 20:20:27 -0400

    routes testing completed

--- Server/Seeders/Seed_Artist.js
+++ Server/Seeders/Seed_Artist.js
@@ -1,6 +1,7 @@
-const db = require('./models')
+const Artist = require('./models')
 
-db.Artist.create([{
+Artist.create([{
+    id: '1',
     name: 'Kyle Partin',
     Moniker: 'Cut Rugs',
     art_type: 'Electronic',
@@ -9,6 +10,27 @@ db.Artist.create([{
     email: 'boy@gmail.com',
     agency: 'The Undergrowth',
     links: "pewpew"
+}, {
+    id: '2',
+    name: 'Donkey Doug',
+    Moniker: 'June Bugs',
+    art_type: 'Tectronic',
+    genre: 'Exp. Oboes',
+    phone: '888-878-8888',
+    email: 'boyoboy@gmail.com',
+    agency: 'The Undercarriage',
+    links: "pewdipew"
+}, {
+    
+    id: '3',
+    name: 'Twi',
+    Moniker: 'Bugsy',
+    art_type: 'Tecno',
+    genre: 'Super Oboes',
+    phone: '888-868-8888',
+    email: 'girl@gmail.com',
+    agency: 'The Overcarriage',
+    links: "placeholder"
 }])
 .then(() => {
     console.log('Success!')
--- Server/server.js
+++ Server/server.js
@@ -1,17 +1,40 @@
-// require('dotenv').config()
+// Dependencies (?)
 const express = require('express')
-require('dotenv').config()
+const cors = require('cors')
 const mongoose = require('mongoose')
+const artistController = require('./Controllers/Artist_Controller')
+// if (process.env.node !== 'production') {}
+require('dotenv').config();
 
 const app = express()
 
+// Middleware?
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }))
+
+
+// Controllers
+app.use('/artists', require('./Controllers/Artist_Controller'))
+
+// Route for pages NOT handled by OUR routes.
+app.get('*', (req, res) => {
+  res.send('error404')
+})
+
+// DB connection 
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true, 
+  useNewUrlParser: true,
   useUnifiedTopology: true
 })
 
-mongoose.connection.on('connected', function () {  
-    console.log('Mongoose default connection open to ' + process.env.MONGO_URI);
-  }); 
+// DB connection test
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose default connection open to ' + process.env.MONGO_URI);
+});
 
-app.listen(3001)
+// Listener
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
